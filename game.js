8e1cd344f67a71b8d2a30f3888fc45e4c74bf4b1
ryanohs <rohs.pub@gmail.com>
2015-11-24 22:45:27 -0600

    ..

--- game.js
+++ game.js
@@ -48,9 +48,8 @@ var Game = function(canvas, instance, fps){
     isMouseDown = true;
     e.preventDefault();
     var mouse = {
-      // todo need to check for touches property
-      x: e.pageX - canvas.offsetLeft,
-      y: e.pageY - canvas.offsetTop,
+      x: e.pageX || e.touches[0].pageX - canvas.offsetLeft,
+      y: e.pageY || e.touches[0].pageY - canvas.offsetTop,
       isMouseDown: isMouseDown
     };
     if(instance.mouseDown) instance.mouseDown(mouse);
@@ -60,9 +59,8 @@ var Game = function(canvas, instance, fps){
     isMouseDown = false;
     e.preventDefault();
     var mouse = {
-      // todo need to check for touches property
-      x: e.pageX - canvas.offsetLeft,
-      y: e.pageY - canvas.offsetTop,
+      x: e.pageX || e.touches[0].pageX - canvas.offsetLeft,
+      y: e.pageY || e.touches[0].pageY - canvas.offsetTop,
       isMouseDown: isMouseDown
     };
     if(instance.mouseUp) instance.mouseUp(mouse);
@@ -71,9 +69,8 @@ var Game = function(canvas, instance, fps){
   var mouseXY = e => {
     e.preventDefault();
     var mouse = {
-      // todo need to check for touches property
-      x: e.pageX - canvas.offsetLeft,
-      y: e.pageY - canvas.offsetTop,
+      x: e.pageX || e.touches[0].pageX - canvas.offsetLeft,
+      y: e.pageY || e.touches[0].pageY - canvas.offsetTop,
       isMouseDown: isMouseDown
     };
     if(instance.mouseXY) instance.mouseXY(mouse);
