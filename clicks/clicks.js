99cbdec8139539341f8ac5c9323e052d0aba50d9
ryanohs <rohs.pub@gmail.com>
2015-11-25 20:33:10 -0600

    colors

--- clicks/clicks.js
+++ clicks/clicks.js
@@ -16,8 +16,11 @@ var Circle = function(x, y, color) {
   this.x = x;
   this.y = y;
   this.r = 2;
-  this.maxr = 60;
+  this.maxr = 120;
   this.remove = false;
+
+  this.colorStepS = (100 - color.s)/this.maxr;
+  this.colorStepL = (100 - color.l)/this.maxr;
 
   this.step = () => {
     this.r++;
@@ -29,7 +32,8 @@ var Circle = function(x, y, color) {
   this.render = () => {
     scene.beginPath();
     scene.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-    scene.strokeStyle = color;
+    var hsl = "hsl(" + color.h + ", " + (color.s + this.colorStepS * this.r) + "%, " + (color.l + this.colorStepL * this.r) + "%)";
+    scene.strokeStyle = hsl;
     scene.lineWidth = 2;
     scene.stroke();
     scene.closePath();
@@ -40,34 +44,19 @@ var Clicks = function(dots) {
   this.game; // injected
 
   var objects = [];
-
+  var hue = 180;
 
   this.mouseDown = mouse => {
     this.mouseXY(mouse);
   }
+
   this.mouseXY = mouse => {
     if(mouse.isMouseDown) {
-      objects.push(new Circle(mouse.x, mouse.y, "green"));
+      var color = {h: hue++, s: 75, l: 75};
+      if(hue == 360) hue = 0;
+      objects.push(new Circle(mouse.x, mouse.y, color));
     }
   }
-
-  this.keydown = e => {
-    switch(e.keyCode)
-    {
-      case 37:
-        this.left();
-        break;
-      case 38:
-        this.up();
-        break;
-      case 39:
-        this.right();
-        break;
-      case 40:
-        this.down();
-        break;
-    }
-  };
 
   this.step = () => {
     objects.forEach(o => o.step());
@@ -75,8 +64,7 @@ var Clicks = function(dots) {
   };
 
   this.render = () => {
-    scene.fillStyle = "#ccc";
-    scene.fillRect(0,0,canvas.width,canvas.height);
+    scene.clearRect(0,0,canvas.width,canvas.height);
     objects.forEach(o => o.render());
   };
 
